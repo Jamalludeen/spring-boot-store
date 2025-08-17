71fa905429c5dc0d621d8ae87abdad6c2ec8996b
Karimi <jamalghazniwal@gmail.com>
2025-08-17 15:43:05 +0430

    Ioc container

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -2,14 +2,18 @@ package com.example.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
-	public static void main(String[] args) {
-//		SpringApplication.run(StoreApplication.class, args);
-		var orderService = new OrderService();
-		orderService.setPaymentService(new PaypalPaymentService());
-		orderService.placeOrder();
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var orderService = context.getBean(OrderService.class);
+        var orderService2 = context.getBean(OrderService.class);
+//        var orderService = context.getBean(HeavyResource.class);
+        orderService.placeOrder();
+//        orderService.placeOrder();
+    }
 }
